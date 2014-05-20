f851a367517de48143cd041af0ca7b118a9ddaa4
Silas Rioux <silas.rioux@gmail.com>
2014-05-20 17:58:46 -0500

    return nil if object's parent is a blank object

--- lib/her/model/associations/association.rb
+++ lib/her/model/associations/association.rb
@@ -44,7 +44,9 @@ module Her
 
         # @private
         def fetch(opts = {})
-          return @opts[:default].try(:dup) if @parent.attributes.include?(@name) && @parent.attributes[@name].empty? && @params.empty?
+          #return default if parent is a blank object.
+          #also return default if parent attributes includes a key for the object but the value is blank and no parameters were passed in.
+          return @opts[:default].try(:dup) if @parent.attributes.empty? || (@parent.attributes.include?(@name) && (@parent.attributes[@name].nil? || @parent.attributes[@name].empty?) && @params.empty?)
 
           if @parent.attributes[@name].blank? || @params.any?
             path = build_association_path lambda { "#{@parent.request_path(@params)}#{@opts[:path]}" }
